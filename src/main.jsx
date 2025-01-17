2f141dd6883a7a7e9dc57e6f0b341e189bfaccb3
Dilsha-M <dilsham190@gmail.com>
2025-01-17 10:24:40 +0530

    Create Frontend food delivery website

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Browser}
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    <App />
- 
+    <BrowserRouter>
+        <StoreContextProvider>
+            <App />
+        </StoreContextProvider>
+    </BrowserRouter>
 )
